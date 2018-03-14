5475b8c95931e3ad33be0e019828ee71597db667
almoller <moller.alan@gmail.com>
2018-03-14 13:54:17 -0600

    initial commit

---
+++ assets/app.js
@@ -0,0 +1,9 @@
+var config = {
+    apiKey: "AIzaSyD2vIX58Z1nhStzv_wfpMMc3OSdCrXXDPw",
+    authDomain: "train-scheduler-8f243.firebaseapp.com",
+    databaseURL: "https://train-scheduler-8f243.firebaseio.com",
+    projectId: "train-scheduler-8f243",
+    storageBucket: "train-scheduler-8f243.appspot.com",
+    messagingSenderId: "483164693206"
+};
+firebase.initializeApp(config);
