2031bd7510df91f4e34a1deee5f58becfe3897b3
almoller <moller.alan@gmail.com>
2018-03-19 17:04:03 -0600

    database is now functional

--- assets/app.js
+++ assets/app.js
@@ -1,3 +1,4 @@
+
 var config = {
     apiKey: "AIzaSyD2vIX58Z1nhStzv_wfpMMc3OSdCrXXDPw",
     authDomain: "train-scheduler-8f243.firebaseapp.com",
@@ -5,5 +6,45 @@ var config = {
     projectId: "train-scheduler-8f243",
     storageBucket: "train-scheduler-8f243.appspot.com",
     messagingSenderId: "483164693206"
-};
-firebase.initializeApp(config);
+  };
+  firebase.initializeApp(config);
+
+var database = firebase.database();
+
+var trainName = "";
+var destination = "";
+var firstDepartureTime = "";
+var frequency = 0;
+
+
+
+$("#add-train").on("click", function (event) {
+    event.preventDefault();
+
+    trainName = $("#train-name").val().trim();
+    destination = $("#destination").val().trim();
+    firstDepartureTime = $("#first-departure-time").val().trim();
+    frequency = $("#frequency").val().trim();
+
+    console.log("1: "+trainName+ " 2: "+destination+ " 3: "+firstDepartureTime+ " 4: "+frequency);
+
+    database.ref().push({
+        trainName: trainName,
+        destination: destination,
+        firstDepartureTime: firstDepartureTime,
+        frequency: frequency,
+        // dateAdded: firebase.database.ServerValue.TIMESTAMP
+    });
+});
+
+
+database.ref().on("child_added", function(snapshot){
+    
+    var table = $("#trainSchedule");
+    
+    table.append("<tr>");
+    table.append("<td>" + snapshot.val().trainName);
+    table.append("<td>" + snapshot.val().destination);
+    table.append("<td>" + snapshot.val().frequency);
+    // table.append("<td>" + firstDepartureTime);
+});
